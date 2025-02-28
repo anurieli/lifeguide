11c81e2ddf5059192adf075cbc7a655fb6e6cfd1
Ariel Nurielissh <anurieli365@gmail.com>
2025-02-27 22:07:51 -0500

    ficed web3forms id

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -73,7 +73,7 @@ export default function ContactForm() {
           Accept: "application/json",
         },
         body: JSON.stringify({
-          access_key: "Y3c95f3cd-5bfe-429e-b32a-54ac4e510253",
+          access_key: "3c95f3cd-5bfe-429e-b32a-54ac4e510253",
           name: formData.name,
           email: formData.email,
           message: formData.message,

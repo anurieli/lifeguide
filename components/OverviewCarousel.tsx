1f21b4aff833eb240ddb114f75444e67579dc0cc
Ariel Nurielissh <anurieli365@gmail.com>
2025-03-02 09:36:04 -0500

    fixed session bug

--- components/OverviewCarousel.tsx
+++ components/OverviewCarousel.tsx
@@ -16,7 +16,7 @@ const steps: Step[] = [
   {
     title: 'Sign Up',
     description: 'Either with Google or the old-fashioned way.',
-    subdescription: 'No, I don\'t care about selling your data, I just need to save your progress.',
+    subdescription: 'No, we don\'t care about selling your data, we just need to save your progress!',
     icon: (
       <svg className="w-12 h-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
@@ -26,7 +26,7 @@ const steps: Step[] = [
   {
     title: 'Set Time Aside - Lock In',
     description:'Sit down, deatch for second, prepare to go deep.',
-    subdescription: 'This is the hard part, but it just fucking works. You\'re worth a try.',
+    subdescription: 'This is the hard part, but it just works. You\'re worth a try.',
     icon: (
       <svg className="w-12 h-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
